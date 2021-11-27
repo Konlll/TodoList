05b764c304d6e12b9d550dad2a72a2b7324dd86e
Géczi Kornél <hungarokornel@gmail.com>
2021-11-27 11:33:13 +0100

    Create HTML and CSS template. Save event ready.

---
+++ main.js
@@ -0,0 +1,57 @@
+// Todo Class
+class Todo{
+    constructor(title, isReady, todoId){
+        this.title = title
+        this.isReady = isReady
+        this.todoId = todoId
+    }
+
+    getTitle(){
+        return this.title
+    }
+
+    getReady(){
+        return this.isReady
+    }
+
+    getTodoId(){
+        return this.todoId
+    }
+
+    setReady(x){
+        this.isReady = x
+    }
+
+    appendTemplate(){
+        return `
+            <div class="todo" id="${this.todoId}">
+                <div class="todo-title">
+                    <h2>${this.title}</h2>
+                </div>
+                <div class="todo-events">
+                    <i class="fas fa-check" title="Mark as ready"></i>
+                    <i class="fas fa-times" title="Mark as unready"></i>
+                    <i class="fas fa-trash" title="Delete todo"></i>
+                </div>
+            </div>
+        `
+    }
+}
+
+// Save Event
+const save = (e) => {
+    e.preventDefault()
+    // Get Input Value
+    let todoValue = document.querySelector(".form-input").value
+    // If the value is empty break the function
+    if(!todoValue){
+        return
+    }
+    // Otherwise create new Todo and append it to our HTML
+    todo = new Todo(todoValue, false, Math.floor(Math.random() * 100000000))
+    todosDiv = document.querySelector(".todos")
+    todosDiv.innerHTML += todo.appendTemplate()
+}
+// Save Event Listener
+const saveButton = document.getElementById('save')
+saveButton.addEventListener('click', (e) => save(e))
